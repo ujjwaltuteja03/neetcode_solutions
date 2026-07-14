0ba212d2305a04e41333694ec14e88e725bdc077
ujjwaltuteja03 <ujjwal.tuteja03@gmail.com>
2026-07-14 12:46:41 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        #build adjacency list of prerequisites
+        prereq = {c:[] for c in range(numCourses)}
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
+        
+        # 3 paths for a course:
+        # visited: crs added to output
+        # visiting: crs in cycle but not in output
+        # unvisited: not in cycle or output
+
+        output = []
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            if crs in cycle: return False
+            if crs in visit: return True
+
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if not dfs(pre): return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+        
+        for c in range(numCourses):
+            if not dfs(c): return []
+        return output
